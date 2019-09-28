cc0ef0c30af8f4bfb91df4c197182c1e8cded3ce
Thomas Aylott <oblivious@subtlegradient.com>
2019-09-27 20:57:36 -0400

    windows support ++

--- scripts/venv.sh
+++ scripts/venv.sh
@@ -1,2 +1,2 @@
 cd "$(dirname "$0")/.."
-python3 -m venv python3 && . python3/bin/activate && pip3 install -r requirements.txt && pip3 install --upgrade numpy >/dev/stdout
+python -m venv python3 && . python3/bin/activate && pip3 install -r requirements.txt && pip3 install --upgrade numpy >/dev/stdout
